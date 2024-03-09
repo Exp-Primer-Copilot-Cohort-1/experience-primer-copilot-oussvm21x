cae018196e886a99ab3c320068890be5b2a44c46
Khelifa Saadallah Oussama <85697735+oussvm21x@users.noreply.github.com>
2024-03-09 18:12:39 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,5 +1,1 @@
-// Create web server and listen on port 3000    
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(3000, '')
+// Create web servee
