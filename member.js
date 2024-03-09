c96e14ab746b3f429c3b9ea7cf01b0084e459be2
Khelifa Saadallah Oussama <85697735+oussvm21x@users.noreply.github.com>
2024-03-09 18:01:16 +0000

    Copilot ssecond commit

--- member.js
+++ member.js
@@ -1,11 +1,3 @@
-function skillMemebers() {
-    var members = [
-        { name: 'John', age: 20 },
-        { name: 'Steve', age: 24 },
-        { name: 'Anna', age: 19 }
-    ];
-
-    this.getAllMembers = function () {
-        return members;
-    }
+function skillsMember() {
+    // ...
 }
